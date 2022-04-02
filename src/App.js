df91dc2c6bc725da2bd4bcd81583f1b97aa35ed7
Divyansh Sharma <85435719+Dsharma2002@users.noreply.github.com>
2022-04-02 16:18:13 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import About from './Components/About';
+import Shop from './Components/Shop';
+import Nav from './Components/Nav';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/shop' element={<Shop />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+);
+
+export default App;
---
+++ src/Components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+    return (
+        <div className="About">
+            <h1>About page</h1>
+            <h2>This should appear in About page</h2>
+        </div>
+    );
+}
+
+export default About;
